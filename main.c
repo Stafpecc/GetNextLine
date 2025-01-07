df1866672e209c790d14ca8a17dee6fddf414565
Theo Arini <tarini@z4r6p6.42lyon.fr>
2025-01-07 14:56:26 +0100

    refactor: add commentary fix(utils): strdup --> infinite while add: get next line bonus

--- main.c
+++ main.c
@@ -1,6 +1,32 @@
-#include "get_next_line.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tarini <tarini@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/07 13:22:16 by tarini            #+#    #+#             */
+/*   Updated: 2025/01/07 13:26:25 by tarini           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int	main(void)
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
+
+int main(void)
 {
-	
+	int		fd;
+	char	*line;
+
+	fd = open("41_no_nl", O_RDONLY);
+	while (1)
+	{
+		line = get_next_line(fd);
+		if (line == NULL)
+			break ;
+		printf("%s", line);
+		free(line);
+	}
+	return (0);
 }
